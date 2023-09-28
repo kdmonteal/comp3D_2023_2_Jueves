5fdf00864b0cb4176345c40f7ab67b65766938eb
Daniella Marin <kdmonteal@gmail.com>
2023-09-28 10:00:25 -0500

    GLTF LOADER

--- threejs_cards/src/js/model3d.js
+++ threejs_cards/src/js/model3d.js
@@ -23,3 +23,45 @@ function loadObjMtl(path, nameMTL, nameOBJ) {
 }
 
 loadObjMtl("../models/OBJ_MTL/personaje/", "Rupert.mtl", "Rupert.obj");
+
+function loadGLTF() {
+    // Instantiate a loader
+    const loader = new THREE.GLTFLoader();
+
+    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+    const dracoLoader = new THREE.DRACOLoader();
+    dracoLoader.setDecoderPath('../models/GLTF/');
+    loader.setDRACOLoader(dracoLoader);
+
+    // Load a glTF resource
+    loader.load(
+        // resource URL
+        '../models/GLTF/Duck.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Group
+            gltf.scenes; // Array<THREE.Group>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+        },
+        // called while loading is progressing
+        function (xhr) {
+
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+        },
+        // called when loading has errors
+        function (error) {
+
+            console.log('An error happened');
+
+        }
+    );
+}
+
+loadGLTF();
